8c2e33c16204960ee4c4790f20dbcadc46956cde
OFF <peerapat13th@gmail.com>
2024-09-12 02:04:11 +0700

    stock is done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StockComponent } from './components/stock/stock.component';
 
 export const routes: Routes = [
     {
@@ -18,9 +19,15 @@ export const routes: Routes = [
         path: 'register',
         component: RegisterComponent,
         data: { title: 'Register' },
-    },{
+    },
+    {
         path: 'dashboard',
         component: DashboardComponent,
         data: { title: 'Dashboard' },
+    },
+    {
+        path: 'stock',
+        component: StockComponent,
+        data: { title: 'Stock' },
     },
 ];
