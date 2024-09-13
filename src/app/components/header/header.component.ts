4d113f8d1683258a54325f41c90f380885318e56
OFF <peerapat13th@gmail.com>
2024-09-13 16:15:17 +0700

    logout is done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,31 +1,45 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter, Input, inject } from '@angular/core'
 import { MatMenuTrigger, MatMenu, MatMenuItem } from '@angular/material/menu'
 import { MatIcon } from '@angular/material/icon'
 import { MatIconButton } from '@angular/material/button'
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar'
+//Import auth service
+import { AuthService } from '../../services/auth.service'
+
+//Type for user data
+type UserProfile = {
+  username: string,
+  email: string,
+  role: string,
+  token: string
+}
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss',
-    standalone: true,
-    imports: [
-      MatToolbar, 
-      MatToolbarRow, 
-      MatIconButton, 
-      MatIcon, 
-      MatMenuTrigger, 
-      MatMenu, 
-      MatMenuItem
-    ]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbar,
+    MatToolbarRow,
+    MatIconButton,
+    MatIcon,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem
+  ]
 })
 export class HeaderComponent implements OnInit {
 
+  //DI auth
+  private auth = inject(AuthService)
+
   // สร้างตัวแปรไว้เก็บข้อมูลผู้ใช้งานที่ Login
-  userProfile: any = {
-    "username": "",
-    "email": "",
-    "role": ""
+  userProfile: UserProfile = {
+    username: '',
+    email: '',
+    role: '',
+    token: ''
   }
 
   @Output() sidenavToggle = new EventEmitter<void>();
@@ -34,13 +48,9 @@ export class HeaderComponent implements OnInit {
   pageName: string = 'Stock'
   version = '17.3'
 
-  constructor(
-    
-  ) {
-  
-  }
-
   ngOnInit(): void {
+    //show user profile
+    this.userProfile = this.auth.getUser()
   }
 
   onToggleSidenav() {
@@ -48,6 +58,9 @@ export class HeaderComponent implements OnInit {
   }
 
   onClickSignout() {
+    //delete user profile in cookies
+    this.auth.logout()
+    
     window.location.href = '/login'
   }
 
